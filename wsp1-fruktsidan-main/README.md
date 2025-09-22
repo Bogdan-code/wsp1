2595410d20b27ee01c1de324f9a5315ee54ce669
Bogdan-code <65506888+Bogdan-code@users.noreply.github.com>
2025-09-22 20:57:07 +0200

    Add files via upload

---
+++ wsp1-fruktsidan-main/README.md
@@ -0,0 +1,108 @@
+# Webbserverprogrammering 1 - Övning 2
+Exempel-projekt nummer 2 med övningar till kursen Webbserverprogrammering 1 på NTI Gymnasiet Johanneberg.
+Efter genomgång görs övningar nedan **i par**.
+
+![Skärmbild på sidan vi kommer arbeta med](docs/img/fruktparadiset.png)
+
+## Komma igång
+* Ladda ner repositoriet till mappen Webbserverprogrammering på din dator. Antingen som ZIP (isåfall måste du packa upp ZIP-filen) eller så laddar du ner med: `git clone` Spara mappen till din WSP-mapp dvs. bredvid förra övningen.
+* Installera: `bundle`
+* *Seeda* databasen: `rake seed`
+* Köra: `rake dev`
+* Surfa till: http://localhost:9292
+
+## Länkar
+* https://sqlbolt.com/
+* https://sqlitebrowser.org/
+
+## Intro
+* Titta på webbsidan
+* Titta på alla routes inklusive de som är bara kommentarer
+* Titta på views
+* Databaser
+* Öppna DB Browser får SQLite och testa `SELECT * FROM fruits` och testa SQL för att välja ut en frukt
+
+### Uppgifter Intro
+* Lägg till så att man kan se info om en frukt genom att klicka på frukten i listan. Ett exempel på länk som klicket ska gå till är: http://localhost:9292/fruits/2
+
+## Genomgång 1
+* Visa routes `GET /fruits` och `/views/fruits/index.erb`
+* Visa `layout.erb`
+* Visa SQL:
+    * *DB Browser for SQLite*
+    * *SQLBolt*
+    * `db/seeder.rb`
+    * `db/fruits.sqlite`
+    * `app.rb/db-metoden`
+    * `views/layout.erb`
+    * `db.execute('SELECT * FROM fruits WHERE id=?',id).first`
+
+### Uppgifter 1
+1. Gör *SQLBolt* t.o.m. **övning 2**.
+2. Testa att sortera om frukterna på t.ex. ID. Du behöver uppdatera SQL-koden: `db.execute('SELECT * FROM fruits')`
+3. Lägg till ca 5 nya frukter mha. `db/seeder.rb`. För att spara datan till databasen kör du `rake seed`
+4. Öppna databasfilen i DBBrowser och dubbelkolla så du ser din data även där.
+5. Visa all info om en frukt på routen `'/fruits/:id'`
+6. Lägg till mer data eller funktioner som t.ex.
+    * Visa stjärnor istället för ett nummer för fruktbetyg
+    * Lägg till fler kolumner i databasen som t.ex. vilket land en frukt kommer ifrån eller hur mycket den kostar / kg
+    * Lägg till testdatan mha. *DB Browser*
+    * Lägg till testdatan mha. `db/seeder.rb`. Vad hände med datan du la till i *DB Browser*?
+
+## Genomgång 2
+* Hur hänger allt ihop?
+* `db.execute('SELECT * FROM fruits WHERE id=?',id).first` (igen)
+* Ruby hashes
+* SQL Injections
+
+### Uppgifter 2
+1. Gör *SQLBolt* t.o.m. **övning 10**.
+2. Gör alla uppgifter på #1
+3. Lägg till funktioner du kan komma på till fruktshoppen
+4. Börja kämpa med nästa veckas uppgifter (uppgifter #3)
+
+## Genomgång 3
+* Föreläsningen: **5 - Ta bort frukt + Spara ny frukt**
+* [HTTP Requests & Response + GET & POST](https://ntijoh.github.io/webbserverprogrammeringsboken/#_requests_routing) (till Routes for *CRUD*)
+* [Formulär](https://ntijoh.github.io/webbserverprogrammeringsboken/#_formul%C3%A4r)
+* SQL: [Delete](https://ntijoh.github.io/webbserverprogrammeringsboken/#_action_delete_route_messagesiddelete_method_post) & [Inserts](https://ntijoh.github.io/webbserverprogrammeringsboken/#_insert)
+* Params
+
+### Uppgifter 3
+1. Gör en knapp (& formulär) som tar bort en frukt.\
+   Använd routen: `post '/fruits/:id/delete'`
+2. Rita ett sekvensdiagram för hur det ser ut när ni sparar en ny frukt. Bifoga det i din loggbok. Använd: https://www.websequencediagrams.com/app
+3. Lägg till ett formulär för att spara en ny frukt.\
+   Använd routen `GET '/fruits/new'` och filen `new.erb`
+4. Gör routen som sparar datan till databasen: `POST '/fruits/new'`.
+5. Arbeta vidare med *SQLBolt*
+6. Utöka funktionerna.
+    * Utforska och lägg till de funktioner du tycker behövs
+    * Lägg till bilder (eller ikoner) till frukterna
+    * Börja kämpa med nästa veckas uppgifter (#4)
+
+## Genomgång 4
+* Föreläsning: **6 - Uppdatera en frukt**
+* [Uppdatera data](https://ntijoh.github.io/webbserverprogrammeringsboken/#_action_edit_route_messagesidedit_method_get)
+* C.R.U.D.
+* RESTLful
+* Betygskriterier
+
+### Uppgifter 4
+
+1. **Innan ni öppnar datorn** gör ett sekvensdiagram för hur uppdateringen av en frukt går till. Lägg det i loggboken.
+2. Lägg till en ändra-knapp i `index.erb` 
+3. Lägg till ett ändra-formulär i `fruits/edit.erb`
+4. Lägg till så att ändringarna för frukten uppdateras till DB `UPDATE fruits SET name =?...`
+5. Läs igenom betygskriterierna (länk finns i loggboken)
+6. Jobba vidare med *SQLBolt*
+7. Lägg till andra funktioner på fruktsidan. T.ex. kategorier, varukorg (så jag kan köpa) eller andra funktioner du tycker saknas.
+8. Skriv i loggboken. Läs igenom och se om ert sekvensdiagram fortfarande stämmer.
+
+## Genomgång 5
+* Todo
+
+### Uppgifter 5
+* Gör färdigt fruktsidan
+* Jobba med *SQLBolt*
+* Samanfatta i loggboken vad du lärt dig sedan du började Webbserverprogramering. Markera i betygsmatrisen (i loggboken) vad du tycker du kan och inte kan.
---
+++ wsp1-fruktsidan-main/app.rb
@@ -0,0 +1,226 @@
+require 'sinatra'
+require 'securerandom'
+require 'fiddle'
+
+
+
+class App < Sinatra::Base
+
+    # Funktion för att prata med databasen
+    # Exempel på användning: db.execute('SELECT * FROM fruits')
+
+
+    def db
+      return @db if @db
+
+      @db = SQLite3::Database.new("db/fruits.sqlite")
+      @db.results_as_hash = true
+
+      
+      return @db
+    end
+
+    #Kollar igenom alla fruitid's inuti arrayen för att se om den redan finns
+    def checkFruitId(fruitInfo, fruitId)
+      return false if fruitInfo ==[]
+      fruitInfo.each do |i|
+        return true if i[0] == fruitId
+      end
+      return false
+    end
+
+    #Funktion för att sätta ihop en sönder plockad array från formen: a = [[x,y], [z,a]] -> a = "x:y z:a"
+    def arrayIntoCDB(array)
+      newstring = ""
+      array.each do |i|
+        newstring += "#{i[0]}:#{i[1]} "
+      end
+      return newstring
+    end
+
+    def find_count(fruitId)
+      count = 0
+
+      @customerinfo = db.execute('SELECT DISTINCT * FROM customerinfo WHERE customerId=?', session['session_id']).first
+  
+      customerinfo = @customerinfo['selectedFruit']
+
+      fruitInfo = customerinfo.dup.chomp.split(" ").map{|s| s.split(":")}
+
+      fruitInfo.each do |i|
+        if i[0] == fruitId
+          count = i[1]
+        end
+      end
+      return count
+    end
+
+
+
+    #Uppdaterar en specifik frukts antal till count
+    def updateCount(fruitInfo, fruitid, count)
+      newFruitInfo = fruitInfo.dup
+      fruitInfo.each_with_index do |info, i|
+        if info[0] == fruitid
+          newFruitInfo[i][1] = count  
+          return arrayIntoCDB(newFruitInfo)
+        end
+      end
+      raise "Fanns inte en frukt med id:t: #{fruitid} i arrayen: #{fruitInfo}"
+    end
+
+    
+    # Routen gör en redirect till '/fruits'
+    get '/' do
+        redirect('/fruits')
+    end
+    use Rack::Session::Cookie,  :key => 'SESSION_ID'
+                          
+    before do   # Before every request, make sure they get assigned an ID.
+        session[:id] ||= SecureRandom.uuid
+        
+    end
+
+    #Routen hämtar alla frukter i databasen
+    get '/fruits' do
+      @fruits = db.execute('SELECT * FROM fruits')
+      @customers = db.execute('SELECT DISTINCT customerId FROM customerinfo')
+      alreadyExisting = false
+
+      db.execute("INSERT INTO customerinfo (customerId) VALUES (?)", [session['session_id']]) if @customers == [  ]
+      @customers.each do |i|
+
+        if i["customerId"] == session['session_id']
+          alreadyExisting = true
+          break
+        end
+      end
+      if alreadyExisting == false
+        db.execute("INSERT INTO customerinfo (customerId) VALUES (?)", session['session_id'])
+      end
+
+
+
+      erb(:"fruits/index")
+    end
+
+
+    # Routen visar ett formulär för att spara en ny frukt till databasen.
+    get '/fruits/new' do
+      erb(:"fruits/new")
+    end
+
+    #Routen visar din varukorg
+    get '/fruits/cart' do 
+      @fruitsId = db.execute("SELECT id FROM fruits")
+  
+      @cartInfo = db.execute("SELECT DISTINCT selectedFruit FROM customerinfo WHERE customerId=?", session['session_id']).first["selectedFruit"]
+      cartinfo = @cartInfo.chomp.split(" ").map{|s| s.split(":")}
+      
+      @cartArray = []
+
+      cartinfo.each do |info|
+        @fruitsId.each do |id|
+          if id["id"].to_s == info[0]
+            
+            @cartArray << [db.execute("SELECT name FROM fruits WHERE id=?", id["id"]).first["name"], info[1]]
+
+          end
+        end
+      end
+
+
+      erb(:"fruits/cart")
+    end
+
+    # Routen sparar en frukt till databasen och gör en redirect till '/fruits'.
+    post '/fruits' do
+      #todo: Läs ut fruit_name & fruit_description från params
+      fruit_name = params["fruit_name"]
+      fruit_description = params["fruit_description"]
+      fruit_taste = params["fruit_taste"]
+      #todo: Lägg till den nya frukten i databasen
+      db.execute(
+        "INSERT INTO fruits (name, tastiness, description) VALUES (?,?,?)",
+        [fruit_name, fruit_taste,fruit_description]
+      )
+      
+      redirect "/fruits"
+    end
+
+    # Routen visar all info (från databasen) om en frukt
+    get '/fruits/:id' do | id |
+      #todo välj ut frukten med it:t
+      #Visa i rätt ERB-fil
+      @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id).first
+      @count = find_count(id)
+      erb(:"fruits/show")
+    end
+
+    # Routen tar bort frukten med id
+    get '/fruits/:id/delete' do | id |
+      #todo: Ta bort frukten i databasen med id:t
+      @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id).first
+
+      db.execute("DELETE FROM fruits WHERE id=?", id)
+
+      redirect("/fruits")
+
+    end
+
+    post '/fruits/:id/addtocart' do | id |
+      #todo: Ta bort frukten i databasen med id:t
+      @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id).first
+
+      count = params['count'].chomp.to_s
+
+      @customerinfo = db.execute('SELECT DISTINCT * FROM customerinfo WHERE customerId=?', session['session_id']).first
+  
+      customerinfo = @customerinfo['selectedFruit']
+      customerinfo = "" if customerinfo == nil
+      fruitInfo = customerinfo.dup.chomp.split(' ').map{|i| i.split(':')}
+
+
+
+      if !checkFruitId(fruitInfo, id)
+        customerinfo += "#{id}:#{count} "
+
+        db.execute("UPDATE customerinfo SET selectedFruit=? WHERE customerId=?", [customerinfo, session['session_id']])
+      elsif checkFruitId(fruitInfo, id)
+        db.execute("UPDATE customerinfo SET selectedFruit=? WHERE customerId=?", [updateCount(fruitInfo, id, count), session['session_id']])
+      end
+
+      redirect("/fruits/cart")
+
+    end
+
+    # Routen visar ett formulär på edit.erb för att ändra frukten med id
+    get '/fruits/:id/edit' do | id |
+      # todo: Hämta info (från databasen) om frukten med id
+      @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id).first
+
+      # todo: Visa infon i fruits/edit.erb
+      erb(:"fruits/edit")
+    end
+
+    # Routen sparar ändringarna från formuläret
+    post "/fruits/:id/update" do | id |
+      @fruit = db.execute('SELECT * FROM fruits WHERE id=?', id).first
+      # todo: Läs name & category från formuläret
+      fruit_name = params["fruit_name"] if params["fruit_name"].chomp != ""
+      fruit_name = @fruit['name'] if params["fruit_name"].chomp == ""
+      fruit_description = params["fruit_description"] if params["fruit_description"].chomp != ""
+      fruit_description = @fruit['description'] if params["fruit_description"].chomp == ""
+      fruit_taste = params["fruit_taste"] if params["fruit_taste"].chomp != ""
+      fruit_taste = @fruit['tastiness'] if params["fruit_taste"].chomp == ""
+      # todo: Kör SQL för att uppdatera datan från formuläret
+
+      db.execute("UPDATE fruits SET name = ?, tastiness = ?, description = ? WHERE id = ?",
+      [fruit_name, fruit_taste, fruit_description, id])
+
+      redirect "/fruits/#{id}"
+    end
+
+
+    
+end
